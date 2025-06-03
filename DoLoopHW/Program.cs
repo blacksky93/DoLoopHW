a9f60a8e01b5245a7abd8bc11038069d67143652
mgapp <mgapp@MIKEGYO>
2025-06-03 18:14:38 -0400

    Add project files.

---
+++ DoLoopHW/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DoLoopHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? firstName = "";
+            while (firstName.ToLower() != "exit")
+            {
+                Console.Write("What is your name: ");
+                firstName = Console.ReadLine();
+                switch (firstName.ToLower())
+                {
+                    case "tim" or "timothy":
+                        Console.WriteLine($"Hello Professor {firstName}");
+                        break;
+                    case "exit":
+                        break;
+                    default:
+                        Console.WriteLine($"Hello student {firstName}");
+                        break;
+                }
+            };
+        }
+    }
+}
